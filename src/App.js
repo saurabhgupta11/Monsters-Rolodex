59e72d4499001da56f993fe894794ff46c3edacf
Saurabh <saurabhgupta190699@gmail.com>
2020-03-25 12:02:53 +0530

    project files added

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters : [],
+      searchField: ''
+    }
+  }
+  // componentDidMount runs before render() function
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+    .catch((error => console.log(error)))
+  }
+
+  // if you use es5 function syntax then then you have to bind the 'this' keyword manually to our App Component in the constructor(some scoping rules are in play...)
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value});
+  }
+
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return(
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange}/>
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    )
+  }
+
+}
+
+export default App;

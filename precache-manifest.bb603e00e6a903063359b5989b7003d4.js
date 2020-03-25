e33378ee4f3200e392095ffdb5dbffd3275d01d5
Saurabh <saurabhgupta190699@gmail.com>
2020-03-25 13:24:48 +0530

    Updates

---
+++ precache-manifest.bb603e00e6a903063359b5989b7003d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cabcb967eccda5cb10c14735d422410",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "078c171ef1197df79281",
+    "url": "/Monsters-Rolodex/static/css/main.b82918f8.chunk.css"
+  },
+  {
+    "revision": "df8d593596a3ed3d89ae",
+    "url": "/Monsters-Rolodex/static/js/2.8bf845fa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monsters-Rolodex/static/js/2.8bf845fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078c171ef1197df79281",
+    "url": "/Monsters-Rolodex/static/js/main.6e067f2b.chunk.js"
+  },
+  {
+    "revision": "8dbb4e9ef662dab2ad2e",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.ec86702b.js"
+  }
+]);
